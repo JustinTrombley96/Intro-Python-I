5886d6a9bdc32a382b6baed409aaaf726770b60d
Justin Trombley <jtrombley96@gmail.com>
2020-05-04 14:58:33 -0400

    Modules Finished

--- src/modules.py
+++ src/modules.py
@@ -11,24 +11,27 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-print("\n".join(sys.argv))
+print("\n".join(sys.argv)) # 03modules.py
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print(os.name)
-print(sys.platform)
+print(os.name) # nt
+print(sys.platform) #win32 
+
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print(sys.version_info)
+print(sys.version_info) #sys.version_info(major=3, minor=8, micro=2, releaselevel='final', serial=0)
 
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-print(os.getpid())
+print(os.getpid()) #63128
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print(os.getcwd())
-print(os.curdir)
+print(os.getcwd()) #C:\Users\jtrom\OneDrive\Desktop\Projects\Python\Intro-Python-I\src
+ 
 # Print out your machine's login name
 # YOUR CODE HERE
-print(os.getlogin())
+print(os.getlogin()) #jtrom
